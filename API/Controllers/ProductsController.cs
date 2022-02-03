691ac3d67dc13bca96f77a202e3ab958f613e5be
evgiv0 <zhe956@gmail.com>
2022-02-03 20:56:31 +0300

    InitialCommit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return Ok(await _productRepository.GetProductByIdAsync(id));
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrandsAsync()
+        {
+            return Ok(await _productRepository.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypesAsync()
+        {
+            return Ok(await _productRepository.GetProductTypesAsync());
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext storeContext, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!storeContext.ProductBrands.Any())
+                {
+                    var brandsData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    await storeContext.ProductBrands.AddRangeAsync(brands);
+                    await storeContext.SaveChangesAsync();
+                }
+
+                if (!storeContext.ProductTypes.Any())
+                {
+                    var typesData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                    await storeContext.ProductTypes.AddRangeAsync(types);
+                    await storeContext.SaveChangesAsync();
+                }
+
+                if (!storeContext.Products.Any())
+                {
+                    var productsData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                    await storeContext.Products.AddRangeAsync(products);
+                    await storeContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex, "An error iccured on seedind database");
+            }
+        }
+    }
+}
